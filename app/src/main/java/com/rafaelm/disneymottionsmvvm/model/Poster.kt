c79ce0f9ec1d80ae569a83cf48b0662b46fc7f81
RafaelM <RafaelM.Science@gmail.com>
2020-10-27 12:01:49 -0400

    add configure ID extensions

--- app/src/main/java/com/rafaelm/disneymottionsmvvm/model/Poster.kt
+++ app/src/main/java/com/rafaelm/disneymottionsmvvm/model/Poster.kt
@@ -3,6 +3,7 @@ package com.rafaelm.disneymottionsmvvm.model
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 @Entity
 @Parcelize
