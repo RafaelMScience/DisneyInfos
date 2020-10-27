a897c2d639d991278767ae2e6875d63d42619189
RafaelM <RafaelM.Science@gmail.com>
2020-10-27 11:24:45 -0400

    add LiveCoroutinesViewModel.kt and MainViewModel.kt

--- app/src/main/java/com/rafaelm/disneymottionsmvvm/view/ui/main/MainViewModel.kt
+++ app/src/main/java/com/rafaelm/disneymottionsmvvm/view/ui/main/MainViewModel.kt
@@ -1,5 +1,8 @@
 package com.rafaelm.disneymottionsmvvm.view.ui.main
 
+import com.rafaelm.disneymottionsmvvm.base.LiveCoroutinesViewModel
+import com.rafaelm.disneymottionsmvvm.repository.MainRepository
+
 class MainViewModel constructor(
     private val mainRepository: MainRepository
 ) : LiveCoroutinesViewModel() {
